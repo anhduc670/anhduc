fd749cc614861bc66312b0d4bf0b607be4d26789
anhduc670 <20230670@eaut.edu.vn>
2026-03-31 19:57:56 +0700

    Add files via upload

---
+++ phan1_4.py
@@ -0,0 +1,9 @@
+n = int(input("Nhập n (<20): "))
+
+if n >= 20:
+    print("n phải < 20")
+else:
+    print("Các số thỏa mãn:")
+    for i in range(1, n + 1):
+        if i % 5 == 0 or i % 7 == 0:
+            print(i, end=" ")
